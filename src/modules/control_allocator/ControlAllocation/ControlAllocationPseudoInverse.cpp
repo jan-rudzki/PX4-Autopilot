e230e055d5c61d1036aa090a66ef3685e2f22079
jan-rudzki <jan.rudzki@vortex-aerotec.com>
2024-12-06 11:15:19 +0100

    added build artifacts of ros2 ws to gitignore

--- src/modules/control_allocator/ControlAllocation/ControlAllocationPseudoInverse.cpp
+++ src/modules/control_allocator/ControlAllocation/ControlAllocationPseudoInverse.cpp
@@ -67,6 +67,18 @@ ControlAllocationPseudoInverse::updatePseudoInverse()
 		normalizeControlAllocationMatrix();
 
 		_mix_update_needed = false;
+
+		// px4_info the elements of the _mix matrix
+		for (int i = 0; i < _num_actuators; i++) {
+			PX4_INFO("Mix %d: %8.4f %8.4f %8.4f %8.4f %8.4f %8.4f",
+				 i,
+				 (double)_mix(i, 0),
+				 (double)_mix(i, 1),
+				 (double)_mix(i, 2),
+				 (double)_mix(i, 3),
+				 (double)_mix(i, 4),
+				 (double)_mix(i, 5));
+		}
 	}
 }
 
